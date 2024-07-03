5e86f730e1678f427b34ae0788c7170ee82be556
AndersonFBD <andersonf.barrosdias@gmail.com>
2024-07-03 20:20:33 -0300

    revisado parte do routes para ficar de acordo com os arquivos do curso

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,27 +1,30 @@
 const express = require('express');
-const router = express.Router();
-const Job = require('../models/Job');
+const router  = express.Router();
+const Job     = require('../models/Job');
 
+// rota de teste
 router.get('/test', (req, res) => {
-    res.send('acessou')
+  res.send('deu certo');
 });
+
 
 // add job via post
 router.post('/add', (req, res) => {
 
-    let {title, salary, company, description, email, new_job} = req.body;
+  let {title, salary, company, description, email, new_job} = req.body;
+    console.log(req.body);
+  // insert
+  Job.create({
+    title,
+    description,
+    salary,
+    company,
+    email,
+    new_job
+  })
+  .then(() => res.redirect('/'))
+  .catch(err => console.log(err));
 
-    // insert
-    Job.create({
-        title, 
-        description, 
-        salary, 
-        company, 
-        email, 
-        new_job
-    })
-        .then(()=> res.redirect('/'))
-        .catch(err => console.log(err))
 });
 
-module.exports = router;
+module.exports = router
